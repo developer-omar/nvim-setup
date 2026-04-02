bcc6c61ab304e0a65de7e56fb7e801a0307052c7
Omar Enrique <omar.e.geek@gmail.com>
2026-04-01 23:57:02 -0400

    Adding angular schematics for use with oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,18 +5,33 @@
 -- A simple plugin that allows the creation of angular schematics inside your oil window.
 
 return {
-	"stevearc/oil.nvim",
-	---@module 'oil'
-	---@type oil.SetupOpts
-	opts = {
-		view_options = {
-			show_hidden = true,
-		},
+	{
+		"simaxme/oil-angular-schematics.nvim",
+
+		version = "*", -- for stable releases
 	},
-	-- Optional dependencies
-	dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-	keys = {
-		{ "-", "<cmd>Oil<cr>", mode = "n", desc = "Open a parent directory" },
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {
+			view_options = {
+				show_hidden = true,
+			},
+		},
+		-- Optional dependencies
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+		keys = {
+			{ "-", "<cmd>Oil<cr>", mode = "n", desc = "Open A Parent Directory" },
+			{
+				"<leader>las",
+				function()
+					require("oil-angular-schematics").create_angular_schematic()
+				end,
+				mode = "n",
+				desc = "Open Angular Schematics",
+			},
+		},
 	},
 }
